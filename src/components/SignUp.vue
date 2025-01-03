12ad5351cff7def7718f461e75dd647e3da3ff74
tony.tsui <tonytsui.sbhk@gmail.com>
2025-01-03 12:27:47 +0800

    add firebase & google auth

---
+++ src/components/SignUp.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(getAuth(), provider).then((result) => {
+        console.log('User signed in successfully:', result.user);
+    }).catch((error) => {
+        console.error('Error signing in with Google:', error);
+    });
+}
+</script>
+<template>
+    <div class="container-fluid ">
+        <form class="mx-auto ">
+            <h4 class="text-center">Sign Up</h4>
+            <div class="mb-3 mt-5">
+                <label for="" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+                <div id="emailHelp" class="form-text mt-3">Forget password ?</div>
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-5">Sign Up</button>
+        </form>
+        <div class="social-media">
+            <button @click="signInWithGoogle" class="btn btn-primary mt-5">Google</button>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+    apiKey: "AIzaSyD8mn8ne4YBLU6qcdn_qJisA03PvKGQaso",
+    authDomain: "ssf-web-ab742.firebaseapp.com",
+    projectId: "ssf-web-ab742",
+    storageBucket: "ssf-web-ab742.firebasestorage.app",
+    messagingSenderId: "1014915914617",
+    appId: "1:1014915914617:web:2cd79bebb883f1c4c36da8",
+    measurementId: "G-J47LXF42V7"
+};
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 const app = createApp(App)
 
 app.use(createPinia())
